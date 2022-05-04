117cffa2f07d1b60cbf864ee412f4b6aef6f04a5
Jefferson Ferreira <jefferson14489@gmail.com>
2022-05-04 16:34:47 -0300

    aula 02

---
+++ main.js
@@ -0,0 +1,17 @@
+// const navigation = document.getElementById("navigation");
+function onScroll() {
+  if (scrollY == 0) {
+    navigation.classList.remove("scroll");
+  } else {
+    navigation.classList.add("scroll");
+  }
+}
+
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
